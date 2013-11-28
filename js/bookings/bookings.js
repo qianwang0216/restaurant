86d5b0817eed6e9eff9a80c2e3c0c0952eddcfb6
djuta <devenjuta@gmail.com>
2013-11-28 17:01:38 -0500

    Added some giftcard functionality/ Indika's stuff

---
+++ js/bookings/bookings.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+
+function display(obj,id1,id2,id3) {
+ txt = obj.options[obj.selectedIndex].value;
+ document.getElementById(id1).style.display = 'none';
+ document.getElementById(id2).style.display = 'none';
+  document.getElementById(id3).style.display = 'none';
+ if ( txt.match(id1) ) {
+ document.getElementById(id1).style.display = 'block';
+ }
+ if ( txt.match(id2) ) {
+ document.getElementById(id2).style.display = 'block';
+ }
+ if ( txt.match(id3) ) {
+ document.getElementById(id3).style.display = 'block';
+ }
+ }
+ 
+ //--------------------------------------
+ 
+  function show() { 
+        if(document.getElementById('detail').style.display=='none') { 
+            document.getElementById('detail').style.display='block'; 
+        } 
+        return false;
+    } 
+    function hide() { 
+        if(document.getElementById('detail').style.display=='block') { 
+            document.getElementById('detail').style.display='none'; 
+        } 
+        return false;
+    }   
+	
+	//----------------------------------------
+	
+	function leaveChange() {
+    if (document.getElementById("Sel_Time").value != "six"){
+        document.getElementById("message").innerHTML = "Table available for : "+p_size.value +" On "+pic_date.value;
+		
+    }     
+    else{
+        document.getElementById("message").innerHTML = "Sorry all tables occupied";
+    }        
+}
+
+//----------------------------------------------
+
+function thankmsg() {
+      document.getElementById("msg1").innerHTML = "Thank you for your reservation. Hope to see you soon....";
+		
+    }   
--- js/giftcard.js
+++ js/giftcard.js
@@ -4,14 +4,17 @@ function changeCard(card)
 	switch(card.id)
 	{
 		case 'gcyellow':
-			document.getElementById('gcpreview').style.backgroundColor = 'yellow';
+			document.getElementById('gcpreview').style.backgroundColor = '#681207';
+			document.getElementById('gcpreview').style.color = '#fff9e8';
 
 			break;
 		case 'gcred':
-			document.getElementById('gcpreview').style.backgroundColor = 'red';
+			document.getElementById('gcpreview').style.backgroundColor = '#fff9e8';
+			document.getElementById('gcpreview').style.color = '#681207';
 			break;
 		case 'gcbw':
-			document.getElementById('gcpreview').style.backgroundColor = 'black';
+			document.getElementById('gcpreview').style.backgroundColor = '#d22a09';
+			document.getElementById('gcpreview').style.color = '#fff9e8';
 			break;
 	}
 }
@@ -20,6 +23,6 @@ function cardInfo()
 {
 	document.getElementById('prefname').innerHTML = document.getElementById("rfname").value
 	document.getElementById('prelname').innerHTML = document.getElementById("rlname").value
-	document.getElementById('preprice').innerHTML = document.getElementById("card-amt").value
+	document.getElementById('preprice').innerHTML = "$"+document.getElementById("card-amt").value
 }	
 
